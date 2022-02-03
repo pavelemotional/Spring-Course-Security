fdb7a3f7caeffdd4089b14d13ae9a0ec16d097be
pavel.emotional <pavel.emotional@gmail.com>
2022-02-03 08:00:08 +0300

    init commit

---
+++ src/main/java/ru/emotional/spring/security/configuration/SecurityConfig.java
@@ -0,0 +1,19 @@
+package ru.emotional.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("pavel").password("pavel").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("ulya").password("ulya").roles("HR"))
+                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+
+    }
+}
---
+++ src/test/java/ru/emotional/spring/security/SpringCourseSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.emotional.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
