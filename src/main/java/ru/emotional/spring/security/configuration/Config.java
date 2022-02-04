9dfd4fbb560513414a15ff38b5381305e22c0fa2
pavel.emotional <pavel.emotional@gmail.com>
2022-02-04 07:52:23 +0300

    Adding DataBase

--- src/main/java/ru/emotional/spring/security/configuration/Config.java
+++ src/main/java/ru/emotional/spring/security/configuration/Config.java
@@ -1,20 +1,24 @@
 package ru.emotional.spring.security.configuration;
 
 
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.sql.Driver;
+
 
 @Configuration
 @ComponentScan(basePackages = "ru.emotional.spring.security")
 @EnableWebMvc
 public class Config {
-    
-//    @Bean
-//    public ViewResolver viewResolver(){
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setPrefix("/templates/");
-//        viewResolver.setSuffix(".html");
-//        return viewResolver;
-//    }
+
+
+
 }
--- src/main/java/ru/emotional/spring/security/configuration/SecurityConfig.java
+++ src/main/java/ru/emotional/spring/security/configuration/SecurityConfig.java
@@ -1,24 +1,29 @@
 package ru.emotional.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("pavel").password("pavel").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("ulya").password("ulya").roles("HR"))
-                .withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
-
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
@@ -29,6 +34,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/manager_info/**").hasRole("MANAGER")
                 .and()
                 .formLogin().permitAll();
-
     }
 }
